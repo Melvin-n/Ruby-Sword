f69bdf051b1c4dfbb9ddd61c499519b63342cbd6
melvin <melvinn721@gmail.com>
2021-11-25 19:19:57 +1300

    added walking animation, updated sprite, added bg

--- ruby-sword.py
+++ ruby-sword.py
@@ -2,10 +2,10 @@ import pygame
 import os
 
 # define size constants
-WIDTH = 600
+WIDTH = 900
 HEIGHT = 450
 
-FPS = 20
+FPS = 60
 SPEED = 5
 
 # color variables
@@ -16,58 +16,86 @@ GREEN = (34, 177, 76)
 SPRITE_SHEET_BG = (192, 192, 192)
 
 
-SPRITE_HEIGHT = 60
-SPRITE_WIDTH = 45
+SPRITE_HEIGHT = 80
+SPRITE_WIDTH = 80
 
-#isWalking = False
-
+# create screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-sprite_sheet = pygame.image.load((os.path.join('cs50-final', 'spritesheet-sword.png'))).convert_alpha()
-
+# set caption
 pygame.display.set_caption('Ruby Sword')
 
-sprite_column = 64
-sprite_row = 64
+# variables for the sprite
+sprite_x = 200
+sprite_y = HEIGHT - 64
+
+#get background image
+bg = pygame.transform.scale(pygame.image.load(os.path.join('cs50-final','bg.png')), (WIDTH, HEIGHT))
 
 
-def get_image(sheet, frame_row, frame_column, width, height, color):
-    # create a surface the size of width and height and assign it to image
-    image = pygame.Surface((width, height)).convert_alpha()
-    # take the sprite sheet and only take the area starting between last param coordinates, from the row/column in the sheet
-    image.blit(sheet, (0, 0), (sprite_column * frame_column, sprite_row * frame_row, width, height))
-    # removes the background color of the spritesheet as per color passed in
-    image.set_colorkey(color)
-    return image
 
-frame_0 = get_image(sprite_sheet, 9, 5, 64, 64, SPRITE_SHEET_BG)
-print(frame_0)
+default_sprite = pygame.transform.scale(pygame.image.load(os.path.join('cs50-final','defaultSprite.png')), (SPRITE_WIDTH, SPRITE_HEIGHT))
+default_sprite.set_colorkey(SPRITE_SHEET_BG)
 
-def draw_window(player, isWalking):
-    screen.fill(WHITE)
-    screen.blit(frame_0, (200, HEIGHT - 64))
- 
+
+# import walking images
+walk_right = [pygame.image.load(os.path.join('cs50-final', 'walking','R_0.png')), pygame.image.load(os.path.join('cs50-final', 'walking','R_1.png')), pygame.image.load(os.path.join('cs50-final', 'walking','R_2.png')), pygame.image.load(os.path.join('cs50-final', 'walking','R_3.png')), pygame.image.load(os.path.join('cs50-final', 'walking','R_4.png')),
+            pygame.image.load(os.path.join('cs50-final', 'walking','R_5.png')), pygame.image.load(os.path.join('cs50-final', 'walking','R_6.png')), pygame.image.load(os.path.join('cs50-final', 'walking','R_7.png')), 
+            pygame.image.load(os.path.join('cs50-final', 'walking','R_8.png'))]
+
+walk_left = [pygame.image.load(os.path.join('cs50-final', 'walking','L_0.png')), pygame.image.load(os.path.join('cs50-final', 'walking','L_1.png')), pygame.image.load(os.path.join('cs50-final', 'walking','L_2.png')), pygame.image.load(os.path.join('cs50-final', 'walking','L_3.png')), pygame.image.load(os.path.join('cs50-final', 'walking','L_4.png')),
+            pygame.image.load(os.path.join('cs50-final', 'walking','L_5.png')), pygame.image.load(os.path.join('cs50-final', 'walking','L_6.png')), pygame.image.load(os.path.join('cs50-final', 'walking','L_7.png')), 
+            pygame.image.load(os.path.join('cs50-final', 'walking','L_8.png'))]
+
+walk_count = 0
+
+
+def draw_window(player, left, right):
+    global walk_count
+    screen.blit(bg, (0,0))
+    # walk count will cycle through the list of images in walk_right/walk_left
+    # when walk count exceeds images * frame per images, reset to 0 and restart the cycle
+    if walk_count + 1 >= 45:
+        walk_count = 0
+    # if either left or right are true (when left/right keys are pressed), the images from corressponding list will load and cycle through as per walk_count
+    # each image is shown for 5 frames as per the integer division
+    if right:
+        image = walk_right[walk_count//5]
+        image.set_colorkey(SPRITE_SHEET_BG)
+        image = pygame.transform.scale(image, (SPRITE_WIDTH, SPRITE_HEIGHT))
+        screen.blit(image, (player.x, player.y))
+        walk_count += 1
+        print('right')
+    elif left:
+        image = walk_left[walk_count//5]
+        image.set_colorkey(SPRITE_SHEET_BG)
+        image = pygame.transform.scale(image, (SPRITE_WIDTH, SPRITE_HEIGHT))
+        screen.blit(image, (player.x, player.y))
+        walk_count += 1
+        print('yes')
+    # if neither left or right is true, load default sprite
+    else:
+        screen.blit(default_sprite, (player.x, player.y))
     pygame.display.update()
 
 
-# def sprite_movement(keys_pressed, player, isWalking): 
-#     #horizontal movement, make sure sprite is not past screen boundaries
-    
-    
-
-
 def main():
-    player = pygame.Rect(200, HEIGHT - SPRITE_HEIGHT,SPRITE_WIDTH, SPRITE_HEIGHT)
+    player = pygame.Rect(sprite_x, HEIGHT - SPRITE_HEIGHT, SPRITE_WIDTH, SPRITE_HEIGHT)
     run = True
-    isJump = False
+    is_jump = False
     vel_y = 15
-    global isWalking
-    isWalking = False
+    clock = pygame.time.Clock()
+ 
+    # walking animation
+    left = False
+    right = False
     
+    
+
     while run:
         clock = pygame.time.Clock()
         clock.tick(FPS)
-        draw_window(player, isWalking)
+        draw_window(player, left, right)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -75,26 +103,32 @@ def main():
                 pygame.quit()
         
         keys_pressed = pygame.key.get_pressed()
-        # sprite_movement(keys_pressed, player, isWalking)
+        # sprite_movement(keys_pressed, player)
+        # left and right flags are used for walk animations
         if(keys_pressed[pygame.K_LEFT] and player.x > 0):
-            isWalking = True
-            player.x -= SPEED    
-        if(keys_pressed[pygame.K_RIGHT] and player.x < WIDTH - SPRITE_WIDTH):
+            player.x -= SPEED  
+            left = True 
+            right = False            
+        elif(keys_pressed[pygame.K_RIGHT] and player.x < WIDTH - SPRITE_WIDTH):
             player.x += SPEED
-            isWalking = True
+            left = False
+            right = True
         else:
-            isWalking = False
+            right = False
+            left = False
+            walk_count = 0
+        #check for jump
+        if is_jump == False and keys_pressed[pygame.K_UP]:
+            is_jump = True
 
-        if isJump == False and keys_pressed[pygame.K_UP]:
-            isJump = True
-        if isJump == True:
-            isWalking = False
+            walk_count = 0
+        if is_jump == True:
             if vel_y >= -15:
                 player.y -= vel_y
                 vel_y -= 1
             else:
                 vel_y = 15
-                isJump = False
+                is_jump = False
 
 
 main()
